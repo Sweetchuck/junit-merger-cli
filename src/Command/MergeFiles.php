635fc311a82283e5a6e258e8c87bfd733d1b5025
Dávid Andor <david-andor@kozpontiagy.hu>
2021-11-17 11:20:27 +0100

    Issue #2 - Disable output formatter

--- src/Command/MergeFiles.php
+++ src/Command/MergeFiles.php
@@ -6,7 +6,7 @@ namespace Sweetchuck\JunitMergerCli\Command;
 
 use Psr\Log\LoggerAwareInterface;
 use Psr\Log\LoggerAwareTrait;
-use SplFileObject;
+use Sweetchuck\JunitMerger\JunitMergerInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
@@ -49,7 +49,7 @@ class MergeFiles extends Command implements ContainerAwareInterface, LoggerAware
             )
             ->addOption(
                 'handler',
-                'p',
+                'a',
                 InputOption::VALUE_REQUIRED,
                 'Allowed values: ' . implode(', ', $this->handlerAllowedValues),
                 'substr',
@@ -59,37 +59,6 @@ class MergeFiles extends Command implements ContainerAwareInterface, LoggerAware
                 InputArgument::OPTIONAL | InputArgument::IS_ARRAY,
                 'JUnit XML filenames to merge into one file. By default filenames will be read from the stdInput.',
             );
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    protected function execute(InputInterface $input, OutputInterface $output)
-    {
-        try {
-            $this->validate($input);
-        } catch (\Exception $e) {
-            $this->logger->error($e->getMessage());
-
-            return max($e->getCode(), 1);
-        }
-
-        $xmlFiles = new \ArrayIterator($input->getArgument('input-files'));
-        if (!$xmlFiles->count()) {
-            $xmlFiles = new SplFileObject('php://stdin');
-        }
-
-        $outputFile = $input->getOption('output-file') ?: 'php://stdout';
-        $output = new StreamOutput(fopen($outputFile, 'w+'));
-
-        $handler = $input->getOption('handler');
-        $serviceId = "junit_merger.$handler";
-        /** @var \Sweetchuck\JunitMerger\JunitMergerInterface $junitMerger */
-        $junitMerger = $this->container->get($serviceId);
-        $junitMerger->mergeXmlFiles($xmlFiles, $output);
-        fclose($output->getStream());
-
-        return 0;
     }
 
     protected function validate(InputInterface $input)
@@ -107,5 +76,71 @@ class MergeFiles extends Command implements ContainerAwareInterface, LoggerAware
         }
 
         return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        try {
+            $this->validate($input);
+        } catch (\Exception $e) {
+            $this->logger->error($e->getMessage());
+
+            return max($e->getCode(), 1);
+        }
+
+        $inputFiles = $this->createInputFilesIterator($input);
+        $output = $this->createOutput($input);
+        $merger = $this->createMerger($input);
+
+        $merger->mergeXmlFiles($inputFiles, $output);
+        $this->tearDownOutput($output);
+
+        return 0;
+    }
+
+    protected function createInputFilesIterator(InputInterface $input): \Iterator
+    {
+        $inputFiles = $input->getArgument('input-files');
+
+        return count($inputFiles) ?
+            new \ArrayIterator($inputFiles)
+            : new \SplFileObject('php://stdin');
+    }
+
+    protected function createOutput(InputInterface $input): OutputInterface
+    {
+        // @todo Error handling.
+        // @todo Create parent directories.
+        $fileName = $input->getOption('output-file');
+        $fileHandler = $fileName === null || $fileName === '' ?
+            \STDOUT
+            : fopen($fileName, 'w+');
+
+        return new StreamOutput(
+            $fileHandler,
+            OutputInterface::VERBOSITY_VERY_VERBOSE | OutputInterface::OUTPUT_RAW,
+            false,
+        );
+    }
+
+    protected function tearDownOutput(OutputInterface $output)
+    {
+        if ($output instanceof StreamOutput) {
+            fclose($output->getStream());
+        }
+
+        return $this;
+    }
+
+    protected function createMerger(InputInterface $input): JunitMergerInterface
+    {
+        $handler = $input->getOption('handler');
+        $serviceId = "junit_merger.$handler";
+
+        /** @noinspection PhpIncompatibleReturnTypeInspection */
+        return $this->container->get($serviceId);
     }
 }
